96ab1f959afb932cb6f1979f59852e1f3ed5755b
emmanuelotieno <owino044@gmail.com>
2023-03-24 15:27:38 -0700

    Like, comment, subscribe

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,14 +2,17 @@
 
 /**
 * main - main function
+*
 * Return: Always 0
 */
+
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
+
 return (0);
 }
 
