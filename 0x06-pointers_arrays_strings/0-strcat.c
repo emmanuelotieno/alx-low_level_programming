3f779df5df85a3f0fe86bf75a3a951c1def4631a
emmanuelotieno <owino044@gmail.com>
2023-03-30 13:38:42 -0700

    a function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,7 +3,7 @@
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * Return: input value
+ * Return: String concantenation
  */
 char *_strcat(char *dest, char *src)
 {
